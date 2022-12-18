0854457b3bb45c9ceda3aef7977558623342b368
Tim <77175275+DerTev@users.noreply.github.com>
2022-12-18 17:56:33 +0100

    Fix routing again

--- src/web_tev/routes.clj
+++ src/web_tev/routes.clj
@@ -2,6 +2,7 @@
   (:require [compojure.route :as routing]
             [compojure.core :as compojure]
             [clojure.data.json :as json]
+            [compojure.middleware :as middleware]
 
             [web-tev.routes.api :as api-routes]
             [web-tev.routes.root :as root-routes]))
@@ -9,8 +10,9 @@
 (compojure/defroutes routes
                      (compojure/GET "/" [:as req] (-> req
                                                       root-routes/root))
-                     (compojure/GET "/api/random-clojure-function" [:as req] (-> req
-                                                                                 api-routes/random-clojure-function
-                                                                                 json/write-str))
+                     (middleware/wrap-canonical-redirect (compojure/GET "/api/random-clojure-function" [:as req]
+                                                           (-> req
+                                                               api-routes/random-clojure-function
+                                                               json/write-str)))
                      (routing/not-found (-> {:message "404 - Not found"}
                                             json/write-str)))
--- src/web_tev/webserver.clj
+++ src/web_tev/webserver.clj
@@ -1,10 +1,9 @@
 (ns web-tev.webserver
   (:require [org.httpkit.server :as http-srv]
-            [web-tev.routes :as routes]
-            [compojure.middleware :as middleware]))
+            [web-tev.routes :as routes]))
 
 (defn start-webserver
   "Start webserver on `port`"
   [port]
   (println (str "Start webserver on port " port "..."))
-  (http-srv/run-server (middleware/wrap-canonical-redirect routes/routes) {:port port}))
+  (http-srv/run-server routes/routes {:port port}))
